5b31dc02c38869788892c6f33137c5b79eea8042
andresmtzc <andresmtzc@gmail.com>
2023-02-15 08:56:05 -0600

    1
    1

--- scott_files/tabselector.js
+++ scott_files/tabselector.js
@@ -7,7 +7,7 @@ $(document).scroll(function () {
     $('.offload').each(function () {
         var t = $(this).parent().offset().top;
         if (y > t) {
-            $(this).fadeIn().delay(2000).fadeOut();
+            $(this).fadeIn().delay(2000).fadeOut().delay(200000);
             
         } 
 
