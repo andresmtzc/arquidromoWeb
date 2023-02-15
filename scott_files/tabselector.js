c98b3c1cbfabac62a4e524a0fee852e108a25a7e
andresmtzc <andresmtzc@gmail.com>
2023-02-15 13:03:15 -0600

    1
    1

--- scott_files/tabselector.js
+++ scott_files/tabselector.js
@@ -16,6 +16,14 @@ $(document).scroll(function () {
     });
 });
 
+var scroll_active = false;
+var scroll_timer = new Date();
+check_scroll_time();
+
+$(window).scroll(function(){
+  scroll_timer = new Date();
+});
+
 function check_scroll_time(){
   now = new Date();
   if ((now.getTime() - scroll_timer.getTime())/1000 > 3){
@@ -25,3 +33,4 @@ function check_scroll_time(){
   }
   setTimeout(function(){ check_scroll_time() },300);
 }
+
