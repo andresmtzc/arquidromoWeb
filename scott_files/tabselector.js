dc5a9fd86faa463a4f468b3caf2c2ee5b0d340b7
andresmtzc <andresmtzc@gmail.com>
2023-02-16 13:04:14 -0600

    1
    1

--- scott_files/tabselector.js
+++ scott_files/tabselector.js
@@ -1,21 +1,19 @@
 
 
-
 var scroll_active = false;
 var scroll_timer = new Date();
 check_scroll_time();
 
 $(window).scroll(function(){
   scroll_timer = new Date();
+});
 
 function check_scroll_time(){
   now = new Date();
-  if ((now.getTime() - scroll_timer.getTime())/1000 > 3){
-    $('.offload').fadeOut();
+  if ((now.getTime() - scroll_timer.getTime())/500 > 1){
+    $('.offload').fadeIn();
   }else{
-    $('.offload').css("opacity:1");
+    $('.offload').fadeOut();
   }
-  setTimeout(function(){ check_scroll_time() },300);
+  setTimeout(function(){ check_scroll_time() },200);
 }
-
-});
---
+++ scott_files/tabselectorBack.js
@@ -0,0 +1,37 @@
+$(document).scroll(function () {
+
+    var y = $(this).scrollTop();
+
+    // Show element after user scrolls past 
+    // the top edge of its parent 
+    $('.offload').each(function () {
+        var t = $(this).parent().offset().top;
+        if (y > t) {
+            $(this).fadeIn();
+            
+        } 
+        else {
+            $(this).fadeOut();
+        }
+    });
+
+
+var scroll_active = false;
+var scroll_timer = new Date();
+check_scroll_time();
+
+$(window).scroll(function(){
+  scroll_timer = new Date();
+});
+
+function check_scroll_time(){
+  now = new Date();
+  if ((now.getTime() - scroll_timer.getTime())/1000 > 3){
+    $('.offload').fadeOut();
+  }else{
+    $('.offload').css("opacity:1");
+  }
+  setTimeout(function(){ check_scroll_time() },300);
+}
+
+});
